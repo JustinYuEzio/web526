966289598ec8f3448db1d86d3bf2eebf0aaa55d6
HENGYI YU <hengyiyu@usc.edu>
2020-12-06 21:57:21 -0800

    4

--- src/components/forms/NewsletterForm.js
+++ src/components/forms/NewsletterForm.js
@@ -15,9 +15,6 @@ class NewsletterForm extends Component {
 
     return (
       <form className={classNames}>
-        <div className="control control-expanded">
-          <input className="input" type="email" name="email" placeholder="Your best email&hellip;" />
-        </div>
         <div className="control">
           <button className="button button-primary button-block button-shadow" type="submit">{submit}</button>
         </div>
